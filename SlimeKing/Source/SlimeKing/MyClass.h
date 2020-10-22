484880dc69e7ed0b440599d1f9d59398fe45e9d9
KajikawaRiku <66106525+KajikawaRiku@users.noreply.github.com>
2020-10-22 15:30:29 +0900

    1022

---
+++ SlimeKing/Source/SlimeKing/MyClass.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SLIMEKING_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+
+	struct Blue_Struct_c
+	{
+		int score = 1;
+		int rank = 1;
+	};
+};
